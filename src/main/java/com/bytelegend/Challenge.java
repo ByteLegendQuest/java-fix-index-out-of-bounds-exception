0587dbbb3588a1b8dd670e970da4f5e4ee7b6a76
lemon-altxx <2515782282@qq.com>
2022-03-17 12:40:58 +0000

    Update at 2022-03-17T12:40:58.166249Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -11,10 +11,16 @@ public class Challenge {
     }
 
     public static Object firstOrNull(Object[] array) {
+        if (array == null) {
+            return null;
+        }
         return array[1];
     }
 
     public static Object lastOrNull(Object[] array) {
+        if (array == null) {
+            return null;
+        }
         return array[array.length];
     }
 }
