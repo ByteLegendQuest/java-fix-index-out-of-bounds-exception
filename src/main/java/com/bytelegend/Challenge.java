df414fd7b6c08f001ad66bee0c895a6308b1a95c
Yeely0162 <Yeely01621@163.com>
2022-05-15 09:23:31 +0000

    Update at 2022-05-15T09:23:31.131583Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,25 +2,30 @@ package com.bytelegend;
 
 public class Challenge {
     public static void main(String[] args) {
-        System.out.println(firstOrNull(new Object[1]));
-        System.out.println(firstOrNull(new Object[] {1}));
-        System.out.println(firstOrNull(new Object[] {"a", "b", "c"}));
-        System.out.println(lastOrNull(new Object[1]));
-        System.out.println(lastOrNull(new Object[] {1}));
-        System.out.println(lastOrNull(new Object[] {"a", "b", "c"}));
+        System.out.println(firstOrNull(new Object[0]));
+        System.out.println(firstOrNull(new Object[]{1}));
+        System.out.println(firstOrNull(new Object[]{"a", "b", "c"}));
+        System.out.println(lastOrNull(new Object[0]));
+        System.out.println(lastOrNull(new Object[]{1}));
+        System.out.println(lastOrNull(new Object[]{"a", "b", "c"}));
     }
 
     public static Object firstOrNull(Object[] array) {
-        if(array == null){
+        if (array.length == 0) {
             return null;
         }
-        else{
+        else {
             return array[0];
         }
 
     }
 
     public static Object lastOrNull(Object[] array) {
-        return array[array.length-1];
+        if (array.length == 0) {
+            return null;
+        }
+        else {
+            return array[array.length - 1];
+        }
     }
 }
