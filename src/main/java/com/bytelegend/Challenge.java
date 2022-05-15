cb9134ed308e494bbd0d2fc6788ab7cec3069788
Yeely0162 <Yeely01621@163.com>
2022-05-15 09:16:38 +0000

    Update at 2022-05-15T09:16:38.394417Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,16 +2,22 @@ package com.bytelegend;
 
 public class Challenge {
     public static void main(String[] args) {
-        System.out.println(firstOrNull(new Object[0]));
+        System.out.println(firstOrNull(new Object[1]));
         System.out.println(firstOrNull(new Object[] {1}));
         System.out.println(firstOrNull(new Object[] {"a", "b", "c"}));
-        System.out.println(lastOrNull(new Object[0]));
+        System.out.println(lastOrNull(new Object[1]));
         System.out.println(lastOrNull(new Object[] {1}));
         System.out.println(lastOrNull(new Object[] {"a", "b", "c"}));
     }
 
     public static Object firstOrNull(Object[] array) {
-        return array[0];
+        if(array == null){
+            return null;
+        }
+        else{
+            return array[0];
+        }
+
     }
 
     public static Object lastOrNull(Object[] array) {
