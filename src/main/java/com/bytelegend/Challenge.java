319650c3223820c547a028277720f5ee07c0b71e
karolk75 <karolk410410@gmail.com>
2022-06-25 21:11:53 +0000

    Update at 2022-06-25T21:11:53.699599Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -14,13 +14,13 @@ public class Challenge {
         if (array.length == 0) {
             return null;
         }
-        return array[1];
+        return array[0];
     }
 
     public static Object lastOrNull(Object[] array) {
         if (array.length == 0) {
             return null;
         }
-        return array[array.length-1];
+        return array[array.length - 1];
     }
 }
