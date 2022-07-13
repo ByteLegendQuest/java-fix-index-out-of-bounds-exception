09b1a6b56e9dd8a467286d41cb1c54547d8147dc
chesterbachelor <kawalerbartosz@gmail.com>
2022-07-13 09:11:42 +0000

    Update at 2022-07-13T09:11:42.109963Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -3,11 +3,11 @@ package com.bytelegend;
 public class Main {
     public static void main(String[] args) {
         System.out.println(firstOrNull(new Object[0]));
-        System.out.println(firstOrNull(new Object[] {1}));
-        System.out.println(firstOrNull(new Object[] {"a", "b", "c"}));
+        System.out.println(firstOrNull(new Object[]{1}));
+        System.out.println(firstOrNull(new Object[]{"a", "b", "c"}));
         System.out.println(lastOrNull(new Object[0]));
-        System.out.println(lastOrNull(new Object[] {1}));
-        System.out.println(lastOrNull(new Object[] {"a", "b", "c"}));
+        System.out.println(lastOrNull(new Object[]{1}));
+        System.out.println(lastOrNull(new Object[]{"a", "b", "c"}));
     }
 
     public static Object firstOrNull(Object[] array) {
