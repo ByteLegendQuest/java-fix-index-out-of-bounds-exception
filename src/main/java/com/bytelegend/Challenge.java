14498bf9e084f69824779022cc23898c6b6af279
chesterbachelor <kawalerbartosz@gmail.com>
2022-07-13 09:14:17 +0000

    Update at 2022-07-13T09:14:17.919189Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,7 +1,7 @@
 package com.bytelegend;
 
-public class Main {
-    public static void Challenge(String[] args) {
+public class Challenge {
+    public static void main(String[] args) {
         System.out.println(firstOrNull(new Object[0]));
         System.out.println(firstOrNull(new Object[]{1}));
         System.out.println(firstOrNull(new Object[]{"a", "b", "c"}));
