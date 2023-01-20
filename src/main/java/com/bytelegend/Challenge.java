0135366f31986413fc696d4db62578add609379a
Qiqi7-7 <87271755+Qiqi7-7@users.noreply.github.com>
2023-01-20 09:24:55 +0000

    Update at 2023-01-20T09:24:55.067782Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -11,10 +11,16 @@ public class Challenge {
     }
 
     public static Object firstOrNull(Object[] array) {
-        return array[1];
+        if (!array.isempty())
+            return array[0];
+        else 
+            return null;
     }
 
     public static Object lastOrNull(Object[] array) {
-        return array[array.length];
+        if (!array.isempty())
+            return array[array.length - 1];
+        else 
+            return null;
     }
 }
