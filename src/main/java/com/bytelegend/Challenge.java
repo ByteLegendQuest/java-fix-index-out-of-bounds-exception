c561acfa6b85ee5de188055c026de05f5ac538fc
darqlt <stungys@gmail.com>
2023-02-21 00:04:17 +0000

    Update at 2023-02-21T00:04:17.684056Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -11,10 +11,10 @@ public class Challenge {
     }
 
     public static Object firstOrNull(Object[] array) {
-        return array[1];
+        return array.length > 0 ? array[0] : null;
     }
 
     public static Object lastOrNull(Object[] array) {
-        return array[array.length];
+        return array.length > 0 ? array[array.length - 1] : null;
     }
 }
