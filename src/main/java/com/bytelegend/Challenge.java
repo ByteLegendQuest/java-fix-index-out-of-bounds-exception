5fdfeb19d1e84dc1fffca09009dbb90c9e24bc58
mijingduI <727539981@qq.com>
2023-04-19 16:30:56 +0000

    Update at 2023-04-19T16:30:56.110308Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -11,10 +11,16 @@ public class Challenge {
     }
 
     public static Object firstOrNull(Object[] array) {
+        if (array.length <= 1) {
+            return null;
+        }
         return array[1];
     }
 
     public static Object lastOrNull(Object[] array) {
-        return array[array.length];
+        if (array.length == 0) {
+            return null;
+        }
+        return array[array.length -1];
     }
 }
