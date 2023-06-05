e29c28e927034ff940916f5f317d6c22a659e48e
andyliukun <744059787@qq.com>
2023-06-05 06:54:31 +0000

    Update at 2023-06-05T06:54:31.367800Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -3,18 +3,18 @@ package com.bytelegend;
 public class Challenge {
     public static void main(String[] args) {
         System.out.println(firstOrNull(new Object[0]));
-        System.out.println(firstOrNull(new Object[] {1}));
-        System.out.println(firstOrNull(new Object[] {"a", "b", "c"}));
+        System.out.println(firstOrNull(new Object[]{1}));
+        System.out.println(firstOrNull(new Object[]{"a", "b", "c"}));
         System.out.println(lastOrNull(new Object[0]));
-        System.out.println(lastOrNull(new Object[] {1}));
-        System.out.println(lastOrNull(new Object[] {"a", "b", "c"}));
+        System.out.println(lastOrNull(new Object[]{1}));
+        System.out.println(lastOrNull(new Object[]{"a", "b", "c"}));
     }
 
     public static Object firstOrNull(Object[] array) {
-        return array[1];
+        return array.length > 0 ? array[0] : null;
     }
 
     public static Object lastOrNull(Object[] array) {
-        return array[array.length];
+        return array.length > 0 ? array[array.length - 1] : null;
     }
 }
